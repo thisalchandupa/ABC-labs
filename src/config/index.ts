fa8fe693e1b7f399c8726bb7f471a30c33558a46
thisalchandupa <146944086+thisalchandupa@users.noreply.github.com>
2024-03-18 20:56:53 +0530

    more changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const PRODUCT_CATEGORIES = [
   {
-    label: '',
+    label: '123',
     value: 'ui_kits' as const,
     featured: [
       {
@@ -21,7 +21,7 @@ export const PRODUCT_CATEGORIES = [
     ],
   },
   {
-    label: '',
+    label: '123',
     value: 'icons' as const,
     featured: [
       {
--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,7 +46,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'Onboarding',
+        fromAddress: 'onboarding@resend.dev',
         fromName: 'ABC Labs',
       },
       secret: process.env.PAYLOAD_SECRET,
